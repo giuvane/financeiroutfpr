73c723890ffb7d94a208bc933fc43287b981e980
giuvane <giuvane.conti@gmail.com>
2019-11-12 08:09:23 -0300

    Mesagens Spring Security
    Traduzidas mensagens de erro do Spring Security que são apresentadas ao usuário

--- src/main/java/br/edu/utfpr/giuvane/web/ContaBean.java
+++ src/main/java/br/edu/utfpr/giuvane/web/ContaBean.java
@@ -17,91 +17,91 @@ import org.primefaces.model.StreamedContent;
 @Named(value = "contaBean")
 @RequestScoped
 public class ContaBean {
-	private Conta selecionada = new Conta();
-	private List<Conta> lista = null;
-	
-        @Inject
-	private ContextoBean contextoBean;
-	private StreamedContent	arquivoRetorno;
-	private int tipoRelatorio;
-	
-	public String salvar() {
-		this.selecionada.setUsuario(this.contextoBean.getUsuarioLogado());
-		ContaRN contaRN = new ContaRN();
-		contaRN.salvar(this.selecionada);
-		this.selecionada = new Conta();
-		this.lista = null;
-		return null;
-	}
+    private Conta selecionada = new Conta();
+    private List<Conta> lista = null;
 
-	public String excluir() {
-		ContaRN contaRN = new ContaRN();
-		contaRN.excluir(this.selecionada);
-		this.selecionada = new Conta();
-		this.lista = null;
-		return null;
-	}
+    @Inject
+    private ContextoBean contextoBean;
+    private StreamedContent	arquivoRetorno;
+    private int tipoRelatorio;
 
-	public String tornarFavorita() {
-		ContaRN contaRN = new ContaRN();
-		contaRN.tornarFavorita(this.selecionada);
-		this.selecionada = new Conta();
-		return null;
-	}
-	
-	public StreamedContent getArquivoRetorno() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		String usuario = contextoBean.getUsuarioLogado().getLogin();
-		String nomeRelatorioJasper = "contas"; //(_1_)
-		String nomeRelatorioSaida = usuario + "_contas"; //(_2_)
-		RelatorioUtil relatorioUtil = new RelatorioUtil();
-		HashMap parametrosRelatorio = new HashMap(); //(_3_)
-		parametrosRelatorio.put("codigoUsuario", contextoBean.getUsuarioLogado().getCodigo());
-		try {
-			this.arquivoRetorno = relatorioUtil.geraRelatorio(parametrosRelatorio, nomeRelatorioJasper,
-				nomeRelatorioSaida, this.tipoRelatorio);
-		} catch (UtilException e) {
-			context.addMessage(null, new FacesMessage(e.getMessage()));
-			return null;
-		} 
-		return this.arquivoRetorno;
-	}
+    public String salvar() {
+        this.selecionada.setUsuario(this.contextoBean.getUsuarioLogado());
+        ContaRN contaRN = new ContaRN();
+        contaRN.salvar(this.selecionada);
+        this.selecionada = new Conta();
+        this.lista = null;
+        return null;
+    }
 
-	public int getTipoRelatorio() {
-		return tipoRelatorio;
-	}
-	
-	public void setTipoRelatorio(int tipoRelatorio) {
-		this.tipoRelatorio = tipoRelatorio;
-	}
+    public String excluir() {
+        ContaRN contaRN = new ContaRN();
+        contaRN.excluir(this.selecionada);
+        this.selecionada = new Conta();
+        this.lista = null;
+        return null;
+    }
 
-	public Conta getSelecionada() {
-		return selecionada;
-	}
+    public String tornarFavorita() {
+        ContaRN contaRN = new ContaRN();
+        contaRN.tornarFavorita(this.selecionada);
+        this.selecionada = new Conta();
+        return null;
+    }
 
-	public void setSelecionada(Conta selecionada) {
-		this.selecionada = selecionada;
-	}
+    public StreamedContent getArquivoRetorno() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        String usuario = contextoBean.getUsuarioLogado().getLogin();
+        String nomeRelatorioJasper = "contas"; //(_1_)
+        String nomeRelatorioSaida = usuario + "_contas"; //(_2_)
+        RelatorioUtil relatorioUtil = new RelatorioUtil();
+        HashMap parametrosRelatorio = new HashMap(); //(_3_)
+        parametrosRelatorio.put("codigoUsuario", contextoBean.getUsuarioLogado().getCodigo());
+        try {
+                this.arquivoRetorno = relatorioUtil.geraRelatorio(parametrosRelatorio, nomeRelatorioJasper,
+                        nomeRelatorioSaida, this.tipoRelatorio);
+        } catch (UtilException e) {
+                context.addMessage(null, new FacesMessage(e.getMessage()));
+                return null;
+        } 
+        return this.arquivoRetorno;
+    }
 
-	public List<Conta> getLista() {
-		if (this.lista == null) {
-			ContaRN contaRN = new ContaRN();
-			this.lista = contaRN.listar(this.contextoBean.getUsuarioLogado());
-		}
-		return this.lista;
+    public int getTipoRelatorio() {
+            return tipoRelatorio;
+    }
 
-	}
+    public void setTipoRelatorio(int tipoRelatorio) {
+            this.tipoRelatorio = tipoRelatorio;
+    }
 
-	public void setLista(List<Conta> lista) {
-		this.lista = lista;
-	}
+    public Conta getSelecionada() {
+            return selecionada;
+    }
 
-	public ContextoBean getContextoBean() {
-		return contextoBean;
-	}
+    public void setSelecionada(Conta selecionada) {
+            this.selecionada = selecionada;
+    }
 
-	public void setContextoBean(ContextoBean contextoBean) {
-		this.contextoBean = contextoBean;
-	}
+    public List<Conta> getLista() {
+            if (this.lista == null) {
+                    ContaRN contaRN = new ContaRN();
+                    this.lista = contaRN.listar(this.contextoBean.getUsuarioLogado());
+            }
+            return this.lista;
+
+    }
+
+    public void setLista(List<Conta> lista) {
+            this.lista = lista;
+    }
+
+    public ContextoBean getContextoBean() {
+            return contextoBean;
+    }
+
+    public void setContextoBean(ContextoBean contextoBean) {
+            this.contextoBean = contextoBean;
+    }
 
 }
